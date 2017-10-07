37d4c4f503fdc61890fc60b7f547c08c3443b65f
Daniel López <dano@maildots.com>
2017-10-07 03:48:37 -0500

    remove boilerplate

--- src/controllers/MainController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-import { default as Alumno } from "../models/Alumno";
-import path = require("path");
-
-export let index = (req: Request, res: Response) => {
-	res.render('../src/views/index', { title: 'SAES', message: 'SAES' })
-};
