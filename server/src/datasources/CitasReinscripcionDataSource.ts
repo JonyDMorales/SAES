35ce0b1088517ea32295c4f0154c322212ed7b50
Daniel López <dano@maildots.com>
2017-10-13 14:17:53 -0500

    Add visibles

--- server/src/datasources/CitasReinscripcionDataSource.ts
+++ server/src/datasources/CitasReinscripcionDataSource.ts
@@ -1,16 +1,23 @@
 import { default as CitaReinscripcion } from "../models/CitaReinscripcion";
 
+export let visible = {
+	boleta_alumno: 1,
+	fecha_inicio: 1,
+	fecha_limite: 1,
+	_id: 0
+}
+
 export let saveCitaReinscripcion = (citaData: any) => {
 	let citaReinscripcion = new CitaReinscripcion(citaData);
 	return citaReinscripcion.save();
 }
 
 export let getCitaByBoleta = (boleta: number) => {
-	let query = CitaReinscripcion.findOne({ boleta_alumno: boleta });
+	let query = CitaReinscripcion.findOne({ boleta_alumno: boleta }, visible);
  	return query.exec();
 }
 
 export let getAllCitas = () => {
-	let query = CitaReinscripcion.find();
+	let query = CitaReinscripcion.find({}, visible);
  	return query.exec();
 }
--- server/src/datasources/HorarioClasesDataSource.ts
+++ server/src/datasources/HorarioClasesDataSource.ts
@@ -1,11 +1,23 @@
 import { default as HorarioClase } from "../models/HorarioClase";
 
+let visible = {
+	id: 1,
+	profesor: 1,
+	unidad_aprendizaje: 1,
+	id_unidad_aprendizaje: 1,
+	grupo: 1,
+	lugares_disponibles: 1,
+	alumnos_inscritos: 1,
+	horarios: 1,
+	_id: 0
+}
+
 export let getAllHorarios = () => {
-	let query = HorarioClase.find({});
+	let query = HorarioClase.find({}, visible);
  	return query.exec();
 }
 
 export let getHorariosSorted = () => {
-	let query = HorarioClase.find({},{ _id: 0 }).sort({ grupo: 1 });
+	let query = HorarioClase.find({}, visible).sort({ grupo: 1 });
 	return query.exec();
 }
